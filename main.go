88b0a163663db88534914b6f6379bfdda413b0f5
Mark Howe <mvahowe@gmail.com>
2021-01-02 17:54:24 +0100

    Almost unmarshaling JSON

--- main.go
+++ main.go
@@ -6,9 +6,13 @@ import (
 )
 
 func main() {
-	fmt.Println(
-		succinct.DocSetFromJSON(
-			"./test_data/serialize_example.json",
-		),
+	ds, _ := succinct.DocSetFromJSON(
+		"./test_data/serialize_example.json",
 	)
+	for docId := range ds.Docs {
+		seq := ds.Docs[docId].Sequences[ds.Docs[docId].MainId]
+		for blockMap := range seq.BlockArrayMaps {
+			fmt.Println("here", blockMap)
+		}
+	}
 }
--- succinct/structure.go
+++ succinct/structure.go
@@ -2,15 +2,19 @@ package succinct
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
 )
 
-type Block map[string]string
+type BlockStringMap map[string]string
+
+type BlockArrayMap map[string]*ByteArray
 
 type Sequence struct {
-	Type   string
-	Blocks []Block
+	Type            string
+	BlockStringMaps []BlockStringMap `json:"blocks"`
+	BlockArrayMaps  []BlockArrayMap
 }
 
 type Doc struct {
@@ -19,23 +23,52 @@ type Doc struct {
 	Sequences map[string]Sequence
 }
 
+type EnumArrayMap map[string]*ByteArray
+
 type DocSet struct {
-	Id    string
-	Enums map[string]string
-	Docs  map[string]Doc
+	Id           string
+	EnumStrings  map[string]string `json:"enums"`
+	EnumArrayMap EnumArrayMap
+	Docs         map[string]Doc
 }
 
-func DocSetFromJSON(pathString string) (DocSet, error) {
+func DocSetFromJSON(pathString string) (*DocSet, error) {
 	jsonFile, err := os.Open(pathString)
 	if err != nil {
-		return DocSet{}, err
+		return nil, err
 	}
 	defer jsonFile.Close()
 	bytes, _ := ioutil.ReadAll(jsonFile)
 	var suc DocSet
 	err = json.Unmarshal(bytes, &suc)
 	if err != nil {
-		return DocSet{}, err
+		return nil, err
 	}
-	return suc, nil
+	suc.EnumArrayMap = make(EnumArrayMap)
+	for enumLabel, enumB64 := range suc.EnumStrings {
+		ba := NewByteArray(256)
+		ba.fromBase64(enumB64)
+		_ = ba.Trim()
+		suc.EnumArrayMap[enumLabel] = &ba
+	}
+	for docId := range suc.Docs {
+		doc := suc.Docs[docId]
+		for seqId := range doc.Sequences {
+			seq := doc.Sequences[seqId]
+			for blockCount := range seq.BlockStringMaps {
+				fmt.Println("BLOCK")
+				blockArrayMap := BlockArrayMap{}
+				seq.BlockArrayMaps = append(seq.BlockArrayMaps, blockArrayMap)
+				blockStringMap := seq.BlockStringMaps[blockCount]
+				for blockFieldKey, blockFieldValue := range blockStringMap {
+					ba := NewByteArray(256)
+					ba.fromBase64(blockFieldValue)
+					_ = ba.Trim()
+					blockArrayMap[blockFieldKey] = &ba
+					fmt.Println(blockFieldKey, blockArrayMap[blockFieldKey], len(blockArrayMap), len(seq.BlockArrayMaps))
+				}
+			}
+		}
+	}
+	return &suc, nil
 }
