d390699085e79997ac8d66ccba7c7806a356b632
Chris Vaughn <chris.vaughn@life.church>
2021-01-03 05:55:42 -0600

    move unmarshaling to UnmarshalJSON

--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
-	"./succinct"
 	"fmt"
+
+	"./succinct"
 )
 
 func main() {
 	ds, _ := succinct.DocSetFromJSON(
 		"./test_data/serialize_example.json",
 	)
+
 	for docId := range ds.Docs {
 		seq := ds.Docs[docId].Sequences[ds.Docs[docId].MainId]
-		for blockMap := range seq.BlockArrayMaps {
-			fmt.Println("here", blockMap)
+		for i, blockMap := range seq.BlockArrayMaps {
+			fmt.Println("here", i)
+			for k, v := range blockMap {
+				fmt.Println(k, v)
+			}
 		}
 	}
 }
--- succinct/structure.go
+++ succinct/structure.go
@@ -2,34 +2,61 @@ package succinct
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"os"
 )
 
-type BlockStringMap map[string]string
+type EnumArrayMap map[string]*ByteArray
+
+func (e *EnumArrayMap) UnmarshalJSON(b []byte) error {
+	*e = make(EnumArrayMap)
+	var enumStrings map[string]string
+	err := json.Unmarshal(b, &enumStrings)
+	if err != nil {
+		return err
+	}
+	for enumLabel, enumB64 := range enumStrings {
+		ba := NewByteArray(256)
+		ba.fromBase64(enumB64)
+		_ = ba.Trim()
+		(*e)[enumLabel] = &ba
+	}
+	return nil
+}
 
 type BlockArrayMap map[string]*ByteArray
 
+func (bam *BlockArrayMap) UnmarshalJSON(b []byte) error {
+	*bam = make(BlockArrayMap)
+	var blocksStrings map[string]string
+	err := json.Unmarshal(b, &blocksStrings)
+	if err != nil {
+		return err
+	}
+	for blockFieldKey, blockFieldValue := range blocksStrings {
+		ba := NewByteArray(256)
+		ba.fromBase64(blockFieldValue)
+		_ = ba.Trim()
+		(*bam)[blockFieldKey] = &ba
+	}
+	return nil
+}
+
 type Sequence struct {
-	Type            string
-	BlockStringMaps []BlockStringMap `json:"blocks"`
-	BlockArrayMaps  []BlockArrayMap
+	Type           string          `json:"type"`
+	BlockArrayMaps []BlockArrayMap `json:"blocks"`
 }
 
 type Doc struct {
-	Headers   map[string]string
-	MainId    string
-	Sequences map[string]Sequence
+	Headers   map[string]string   `json:"headers"`
+	MainId    string              `json:"mainId"`
+	Sequences map[string]Sequence `json:"sequences"`
 }
 
-type EnumArrayMap map[string]*ByteArray
-
 type DocSet struct {
-	Id           string
-	EnumStrings  map[string]string `json:"enums"`
-	EnumArrayMap EnumArrayMap
-	Docs         map[string]Doc
+	Id           string         `json:"id"`
+	EnumArrayMap EnumArrayMap   `json:"enums"`
+	Docs         map[string]Doc `json:"docs"`
 }
 
 func DocSetFromJSON(pathString string) (*DocSet, error) {
@@ -41,34 +68,5 @@ func DocSetFromJSON(pathString string) (*DocSet, error) {
 	bytes, _ := ioutil.ReadAll(jsonFile)
 	var suc DocSet
 	err = json.Unmarshal(bytes, &suc)
-	if err != nil {
-		return nil, err
-	}
-	suc.EnumArrayMap = make(EnumArrayMap)
-	for enumLabel, enumB64 := range suc.EnumStrings {
-		ba := NewByteArray(256)
-		ba.fromBase64(enumB64)
-		_ = ba.Trim()
-		suc.EnumArrayMap[enumLabel] = &ba
-	}
-	for docId := range suc.Docs {
-		doc := suc.Docs[docId]
-		for seqId := range doc.Sequences {
-			seq := doc.Sequences[seqId]
-			for blockCount := range seq.BlockStringMaps {
-				fmt.Println("BLOCK")
-				blockArrayMap := BlockArrayMap{}
-				seq.BlockArrayMaps = append(seq.BlockArrayMaps, blockArrayMap)
-				blockStringMap := seq.BlockStringMaps[blockCount]
-				for blockFieldKey, blockFieldValue := range blockStringMap {
-					ba := NewByteArray(256)
-					ba.fromBase64(blockFieldValue)
-					_ = ba.Trim()
-					blockArrayMap[blockFieldKey] = &ba
-					fmt.Println(blockFieldKey, blockArrayMap[blockFieldKey], len(blockArrayMap), len(seq.BlockArrayMaps))
-				}
-			}
-		}
-	}
-	return &suc, nil
+	return &suc, err
 }
